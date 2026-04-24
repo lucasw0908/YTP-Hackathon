28036aa9af85b9044ff672330c209f0017ba8661
lucasw0908 <lucasw0908@gmail.com>
2026-04-24 23:50:11 +0800

    bug fixed

--- backend/tests/test_root.py
+++ backend/tests/test_root.py
@@ -2,6 +2,6 @@ from fastapi.testclient import TestClient
 
 
 def test_root(test_client: TestClient):
-    response = test_client.get("/api/")
+    response = test_client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World!"}
