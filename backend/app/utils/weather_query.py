ce0fdd2ad4936f47a0d2fa0772b2b8786d3446be
lucasw0908 <lucasw0908@gmail.com>
2026-04-25 15:34:34 +0800

    update: weather query&stations info

--- backend/app/utils/weather_query.py
+++ backend/app/utils/weather_query.py
@@ -1,31 +1,44 @@
 import requests
+import logging
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel
 
 
-def get_weather(lat: float, lon: float, target_time=None):
+log = logging.getLogger(__name__)
+
+class WeatherStatus(BaseModel):
+    WMO_CODE: str
+    status: Literal["windy", "sunny", "rainy", "large_rainy", "others"]
+
+
+def get_weather(lat: float, lng: float, target_time: datetime):
+    # time format: "2026-04-26 14:00"
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": lat,
-        "longitude": lon,
+        "longitude": lng,
         "current_weather": True,
         "hourly": "weathercode,windspeed_10m",
         "timezone": "auto"
     }
 
+    target_time_str = datetime.strftime(target_time, "%Y-%m-%d %H:%M")
     response = requests.get(url, params=params)
     response.raise_for_status()
     data = response.json()
 
-    weather_code = None
-    wind_speed = None
-
-    if target_time:
-        target_iso = target_time.replace(" ", "T")
-        times = data["hourly"]["time"]
-        
-        if target_iso in times:
-            idx = times.index(target_iso)
-            weather_code = data["hourly"]["weathercode"][idx]
-            wind_speed = data["hourly"]["windspeed_10m"][idx]
+    weather_code: int = ...
+    wind_speed: float = ...
+    
+    target_iso: str = target_time_str.replace(" ", "T")
+    times: list = data["hourly"]["time"]
+    
+    if target_iso in times:
+        idx = times.index(target_iso)
+        weather_code = data["hourly"]["weathercode"][idx]
+        wind_speed = data["hourly"]["windspeed_10m"][idx]
     else:
         weather_code = data["current_weather"]["weathercode"]
         wind_speed = data["current_weather"]["windspeed"]
@@ -33,30 +46,21 @@ def get_weather(lat: float, lon: float, target_time=None):
     return parse_weather_condition(weather_code, wind_speed)
 
 
-def parse_weather_condition(code, wind_speed):
+def parse_weather_condition(code: int, wind_speed: float) -> WeatherStatus:
     if wind_speed > 35.0:
-        return "windy"
-        
-    # WMO code
+        return WeatherStatus(WMO_CODE=code, status="windy")
+    
     # 0-3: 晴天/多雲
     if code in [0, 1, 2, 3]:
-        return "sunny"
+        return WeatherStatus(WMO_CODE=code, status="sunny")
+    
     # 51-63: 毛毛雨/小到中雨
     elif code in [51, 53, 55, 56, 57, 61, 63]:
-        return "rainy"
+        return WeatherStatus(WMO_CODE=code, status="rainy")
+    
     # 65-99: 大雨/暴雨/雷陣雨/冰雹
     elif code in [65, 66, 67, 80, 81, 82, 95, 96, 99]:
-        return "largerainy"
-    else:
-        # idk what is this...
-        return "other"
-
-
-test_lat = 25.0769078
-test_lon = 121.5733101
-
-future_time = "2026-04-26 14:00" 
-forecast = get_weather(test_lat, test_lon, future_time) # 預測
-print(f"({future_time}): {forecast}")
-forecast = get_weather(test_lat, test_lon) # 當下
-print(f"(current): {forecast}")
+        return WeatherStatus(WMO_CODE=code, status="large_rainy")
+    
+    log.warning(f"Unexpect WMO code: {code}")
+    return WeatherStatus(WMO_CODE=code, status="others")
